36af385b9bc6f589918cc544234f4ccb4faeed71
Gustav <64610274+sego-w@users.noreply.github.com>
2023-08-02 20:07:06 +0300

    Kalevipoja Drill
    хараджуку дрилл

--- content.js
+++ content.js
@@ -2,10 +2,21 @@
 //alert("Loe raamatut!!!");
 
 const GENERATEHTML = (pageName) => {
-    return ` <div content-align="center">Instead of sitting and watching ${pageName}, you could take a look at this excerpt from your latest book:
-
+    return ` <img src="images/juku_logo.png">
+    <h1 style="font-size:20px" style="font-family:Lora"><center>Instead of sitting and looking at ${pageName}, you could take a look at this excerpt from your latest book:</center><h1>
+    <break>
+    <p style="font-size:16px:><center>Sõua, laulik, lausa suuga,
+    Sõua laululaevakesta,
+    Pajataja paadikesta —
+    Sõua neid senna kaldale,
+    Kuhu kotkad kuldasõnu,
+    Kaarnad hõbekuulutusi,
+    Luiked vaskseid lunastusi
+    Vanast ajast varistanud,
+    Muiste päivist pillutanud.
+    </center><p>
     
-    <p>hi lol</p>
+    <p>Test dogshit text print screen official</p>
     
     
     
@@ -16,4 +27,14 @@ switch (window.location.hostname) {
     case "www.google.com":
         document.body.innerHTML = GENERATEHTML("Google");
         break;
+    case "www.youtube.com":
+        document.body.innerHTML = GENERATEHTML("YouTube");
+        break;
+    case "www.twitter.com":
+        document.body.innerHTML = GENERATEHTML("Twitter");
+        break;
+    case "www.tiktok.com":
+        document.body.innerHTML = GENERATEHTML("TikTok");
+        break;
+
 }
