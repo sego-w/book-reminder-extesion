548b1b591836b6776917a472a7addfe4410d39b5
Gustav <64610274+sego-w@users.noreply.github.com>
2023-08-02 17:47:29 +0300

    Another solution
    Reverts to different page

--- content.js
+++ content.js
@@ -1,32 +1,18 @@
 
 //alert("Loe raamatut!!!");
 
-//This code requires the use of the PDF.js library
+const GENERATEHTML = (pageName) => {
+    return `
+    
+    <p>hi lol</p>
+    
+    
+    
+    `;
+};
 
-//Include the PDF.js library
-const PDFJS = require('pdfjs-dist');
-
-//Create a PDF Document object
-let pdfDoc = null;
-
-//Create a loading task
-let loadingTask = PDFJS.getDocument('sample.pdf');
-
-//Execute the loading task
-loadingTask.promise.then(function(pdf) {
-pdfDoc = pdf;
-//Get the first page of the PDF
-return pdfDoc.getPage(1);
-}).then(function(page) {
-//Read the text from the page
-return page.getTextContent();
-}).then(function(textContent) {
-//Print out the text
-document.getElementById("texts").innerText = textContent;
-
-});
-
-
-
-
-
+switch (window.location.hostname) {
+    case "www.google.com":
+        document.body.innerHTML = GENERATEHTML;
+        break;
+}
